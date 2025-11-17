f2f207575bf693d8a3b718e30ed930e1ccaf11cb
Eywu <45458185+Eywuu@users.noreply.github.com>
2025-11-17 23:29:27 +0100

    Add lsp_lines configuration for diagnostics

---
+++ lua/plugins/lsp_lines.lua
@@ -0,0 +1,22 @@
+local icons = {
+	[vim.diagnostic.severity.ERROR] = "✘",
+	[vim.diagnostic.severity.WARN]  = "",
+	[vim.diagnostic.severity.HINT]  = "💡",
+	[vim.diagnostic.severity.INFO]  = "",
+}
+
+return {
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			require("lsp_lines").setup()
+				vim.diagnostic.config({
+					virtual_text = false,
+					virtual_lines = true,
+					underline = true,
+					signs = { text = icons },
+					severity_sort = true,
+			})
+	end,
+	}
+}
