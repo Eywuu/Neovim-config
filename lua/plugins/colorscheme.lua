b3270c4e0b30e175d3944d0ce58819457055582c
Eywu <45458185+Eywuu@users.noreply.github.com>
2025-11-19 23:53:53 +0100

    Switch colorscheme from gruvbox to vscode, gruvbox is kept but commented

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,47 @@
 return {
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	priority = 1000,
+	-- 	config = function() 
+	-- 		require("gruvbox").setup({
+	-- 			terminal_colors = false, 
+	-- 			undercurl = true,
+	-- 			underline = true,
+	-- 			bold = true,
+	-- 			italic = {
+	-- 				strings = true,
+	-- 				emphasis = true,
+	-- 				comments = true,
+	-- 				operators = false,
+	-- 				folds = true,
+	-- 			},
+	-- 			strikethrough = true,
+	-- 			invert_selection = false,
+	-- 			invert_signs = false,
+	-- 			invert_tabline = false,
+	-- 			inverse = true, 
+	-- 			contrast = "", 
+	-- 			palette_overrides = {},
+	-- 			overrides = {},
+	-- 			dim_inactive = false,
+	-- 			transparent_mode = false,
+	-- 		})
+	-- 		vim.cmd.colorscheme("gruvbox")
+	-- end,
+	-- }
 	{
-		"ellisonleao/gruvbox.nvim",
+		"Mofiqul/vscode.nvim",
+		lazy = false,
 		priority = 1000,
-		config = function() 
-			require("gruvbox").setup({
-				terminal_colors = false, 
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				inverse = true, 
-				contrast = "", 
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = false,
-			})
-			vim.cmd.colorscheme("gruvbox")
+		config = function()
+			require('vscode').setup({
+				transparent = false,
+				italic_comments = true,
+				disable_nvimtree_bg = false,
+				color_overrides = {},
+				group_overrides = {},
+		})
+		require('vscode').load('dark')
 	end,
 	}
 }
